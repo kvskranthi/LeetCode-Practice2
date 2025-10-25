51e586b9927cf13ff6bf30d266cecc7befcc1e26
kvskranthi <145280280+kvskranthi@users.noreply.github.com>
2025-10-25 14:06:14 +0530

    Time: 0 ms (100%), Space: 42.2 MB (65.38%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        if(digits.isEmpty()){
+            return new ArrayList<>();
+        }
+        List<String> ans = new ArrayList<>();
+        backtrack(digits,0, new StringBuilder(),ans);
+        return ans;
+    }
+    private static final String[] digitToLetters = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    private void backtrack(String digits,int i,StringBuilder sb, List<String> ans){
+        if(i==digits.length()){
+            ans.add(sb.toString());
+            return;
+        }
+        for (final char c: digitToLetters[digits.charAt(i)-'0'].toCharArray()){
+            sb.append(c);
+            backtrack(digits,i+1,sb,ans);
+            sb.deleteCharAt(sb.length()-1);
+        }
+    }
+}
